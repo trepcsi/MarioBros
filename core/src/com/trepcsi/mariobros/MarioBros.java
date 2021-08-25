418071f00e65acc1871ed4967f64c9048f75e53f
Illes Miklos <illes.miklos95@gmail.com>
2021-08-25 18:39:50 +0200

    create game and playscreen objects

--- core/src/com/trepcsi/mariobros/MarioBros.java
+++ core/src/com/trepcsi/mariobros/MarioBros.java
@@ -1,31 +1,26 @@
 package com.trepcsi.mariobros;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.trepcsi.mariobros.screens.PlayScreen;
 
-public class MarioBros extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
+public class MarioBros extends Game {
+
+    public SpriteBatch batch; //only 1 batch allowed (memory) in the whole game!
 
     @Override
     public void create () {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        setScreen(new PlayScreen(this));
     }
 
     @Override
     public void render () {
-        ScreenUtils.clear(1, 0, 0, 1);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        super.render();
     }
 
     @Override
     public void dispose () {
         batch.dispose();
-        img.dispose();
     }
 }
