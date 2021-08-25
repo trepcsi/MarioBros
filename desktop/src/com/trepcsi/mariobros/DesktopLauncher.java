46e1948c28bc2f20da096d4b3a37f1aaaa584f51
Illes Miklos <illes.miklos95@gmail.com>
2021-08-25 18:14:44 +0200

    fix generated setup imports

--- desktop/src/com/trepcsi/mariobros/DesktopLauncher.java
+++ desktop/src/com/trepcsi/mariobros/DesktopLauncher.java
@@ -1,8 +1,7 @@
-package com.trepcsi.mariobros.desktop;
+package com.trepcsi.mariobros;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.trepcsi.mariobros.MarioBros;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
